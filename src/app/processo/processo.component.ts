cefc32a98823fee0d2125f74d05465c7a0b1a861
Nathalia-Felix <nathalia.felix@jamesdelivery.com.br>
2022-05-29 18:21:13 -0300

    Validar cnj e adição da service

--- src/app/processo/processo.component.ts
+++ src/app/processo/processo.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { AbstractControl, FormBuilder, FormGroup, NgForm, Validators } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Subject, takeUntil } from 'rxjs';
+import { ProcessoServece } from '../service/processo.service';
 
 
 
@@ -38,15 +39,34 @@ export class ProcessoComponent implements OnInit {
     private fb: FormBuilder,
     private route: ActivatedRoute,
     private router: Router, 
+    private processoService: ProcessoServece,
   ) { }
-
-
-
 
   ngOnInit(): void {}
 
+  public validarCNJ(): void {
+    let REGEX_CNJ = /\s*\d+\-\d{2}\.\d{4}\.\d\.\d{2}\.\d{4}\s*/;
+    if (REGEX_CNJ.test(this.numeroProcesso)) {
+      this.mensagemErro = '';
+      this.buscarProceso();
+    } else {
+      this.mensagemErro = 'CNJ inválido, por favor insira no formato 0000000-00.0000.0.00.0000';
+    }
+  }
 
 
-  public buscarProceso(): void {}
+  /**
+   * Método que busca o processo pelo número
+   */
+  public buscarProceso(): void {
+  /* Atualmente a api esta dando erro de cors, por este motivo não está buscando na base
+    this.processoService.getProcesso(this.numeroProcesso).subscribe(resp => {
+        this.processor = resp;
+     });
+  */
+   
+    // Buscando processo no mock por motivo de erro de cors na api
+    this.processo =  this.processoService.getProcessoMock(this.numeroProcesso);
+  }
    
 }
